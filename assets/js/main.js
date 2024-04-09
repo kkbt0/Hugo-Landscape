9cbf15fdf70ec2c26afea66a9804f62557c61629
kkbt <0@ftls.xyz>
2024-04-09 16:50:29 +0800

    feat: oklch check

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ console.log(
     'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
     'background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
     'background:transparent'
-  )
+)
 console.log("恐咖兵糖的主页 https://www.ftls.xyz");
 
 function switchTheme() {
@@ -42,17 +42,17 @@ loadButtonScript();
 // }, { once: false });
 
 function setClickOutsideToClose(panel, ignores) {
-	document.addEventListener("click", event => {
-		let panelDom = document.getElementById(panel);
-		let tDom = event.target;
-		for (let ig of ignores) {
-			let ie = document.getElementById(ig)
-			if (ie == tDom || (ie?.contains(tDom))) {
-				return;
-			}
-		}
-		panelDom.classList.add("closed");
-	});
+    document.addEventListener("click", event => {
+        let panelDom = document.getElementById(panel);
+        let tDom = event.target;
+        for (let ig of ignores) {
+            let ie = document.getElementById(ig)
+            if (ie == tDom || (ie?.contains(tDom))) {
+                return;
+            }
+        }
+        panelDom.classList.add("closed");
+    });
 }
 setClickOutsideToClose("nav-menu-panel", ["nav-menu-panel", "nav-menu-switch"])
 
@@ -63,19 +63,33 @@ function setHue(hue) {
     // localStorage.setItem('hue', String(hue))
     const r = document.querySelector(':root')
     if (!r) {
-      return
+        return
     }
     r.style.setProperty('--hue', hue)
 }
-  
 
-let hue = 250;  
-setInterval(function () { 
-    if (hue >= 360) {  
-        hue = 0; 
-      } else {  
-        hue += 10;  
-      }
-      setHue(hue);
-}, 1000);
-  
+// 颜色改变示例
+function supportsOklchColor() {
+    var div = document.createElement('div');
+    div.style.color = 'oklch(0 0 0)';
+    document.body.appendChild(div);
+    var isSupported = getComputedStyle(div).color === 'oklch(0 0 0)';
+    document.body.removeChild(div);
+    return isSupported;
+}
+
+if (!supportsOklchColor()) {
+    console.log('Your browser does not support oklch color space.');
+} else {
+    console.log('Your browser supports oklch color space.');
+    let hue = 250;
+    setInterval(function () {
+        if (hue >= 360) {
+            hue = 0;
+        } else {
+            hue += 10;
+        }
+        setHue(hue);
+    }, 1000);
+
+}
